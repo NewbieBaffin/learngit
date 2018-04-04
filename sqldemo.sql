8e4fae04fbd667e3e4c25da3439a013fd307ad4d
Baffin <baffinfeng@outlook.com>
2018-04-04 15:22:16 +0800

    first commit

---
+++ sqldemo.sql
@@ -0,0 +1,10 @@
+create table sale(saledate date,saleamount number(10,2));
+alter session set nls_date_formate='yyyy-mm-dd';
+begin
+	for i in 1..12 loop
+		insert into sale values ('2016-'||i||'-'||i,round((i+dbmx_random.value)*1000,2));
+		insert into sale values ('2017-'||i||'-'||i,round((i+dbmx_random.value)*1000,2));
+	end loop;
+	
+end;
+commit;	
